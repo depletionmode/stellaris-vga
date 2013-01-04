215a08125dac319946e4b74960905f26c0b78eca
David Kaplan <david@2of1.org>
2013-01-04 06:44:03 +0200

    Initial

---
+++ main.c
@@ -0,0 +1,55 @@
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "driverlib/debug.h"
+#include "driverlib/fpu.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/timer.h"
+#include "utils/uartstdio.h"
+/*
+void Timer0IntHandler()
+{
+  TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+  
+  asm("nop");
+}*/
+
+void main()
+{
+  /* run at 40 mhz */
+  SysCtlClockSet(SYSCTL_SYSDIV_5 |
+                 SYSCTL_USE_PLL |
+                 SYSCTL_OSC_MAIN |
+                 SYSCTL_XTAL_16MHZ);
+  
+  /* enable & configure timer + interrupt*/
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+  IntMasterEnable();
+  TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);
+  TimerLoadSet(TIMER0_BASE,
+               TIMER_A,
+               (unsigned int)(SysCtlClockGet() / 1000 / 1000 * 26.4));     /* 26.4us (1056 cycles @ 40 mhz) */
+  IntEnable(INT_TIMER0A);
+  TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);
+  TimerEnable(TIMER0_BASE, TIMER_A);
+  
+  /* enable led */
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+  GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1);
+  
+  while (1)
+  {
+    /* blink led */
+    int i = 0xffff;
+    
+    while (i--);
+    
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 1);
+    SysCtlDelay(SysCtlClockGet() / 1000);
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0);
+  }
+}
---
+++ vga.s
@@ -0,0 +1,74 @@
+  MODULE Timer0IntHandler
+  PUBLIC Timer0IntHandler
+
+  RSEG CODESEG : CODE (2)
+  
+  CODE16
+
+Timer0IntHandler:
+  ; r12 is used as a counter (make sure not using in main code)
+  ; clear interrupt timer
+
+  ; horizontal timing
+  ; hfp 40 (1-40)
+  ; hsp 128 (41-168)
+  ; hbp 88 (169-256)
+  ; hpx 800 (257-1056)
+  ; total = 1056
+  
+  ; vertical timing
+  ; vsp 4 (1-4)
+  ; vbp 23 (5-27)
+  ; vln 600 (28-627)
+  ; vfp 1 (628)
+  ; total = 628
+  
+  ; todo save regs
+  push r1
+  push r2
+  
+  ; sync interrupt latency to hfp
+  
+  ; hsp and vsp
+  ; set hsp low
+  ; load registers from ram
+  
+  ; vsync on (vsp low)
+  ; set vsp low
+  
+  ; vsync off at line 4
+  
+  ; activate pixels on line 27
+  
+  ; deactivate piels on line 628
+  
+  ; save registers to sram
+  
+  ; hsync off
+  ; set hsp high
+  
+  ; ***
+  ; hbp
+  ; ***
+  ; exit on vblank
+  ; else nops
+  
+  ; ***
+  ; hpx
+  ; ***
+  
+  ; can output color on every cycle (each cycle is a pixel)
+  ; nops between pixel change
+  
+  ; hblank
+  ; clear rgbi port
+  
+  ; resore regs
+  pop r2
+  pop r1
+  
+  ;return from interrupt
+  
+  nop
+
+  END
